c48dd117f505850a1511c62c1268a19353e92be3
Md. Abdullah Al Fahim <115242534+abdullahfahim2@users.noreply.github.com>
2024-01-18 15:08:49 +0600

    studentinfo.sql

--- studentinfo.sql
+++ studentinfo.sql
@@ -5,11 +5,10 @@
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
 
 
 CREATE TABLE `studentinfo` (
@@ -20,11 +19,9 @@ CREATE TABLE `studentinfo` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-
 INSERT INTO `studentinfo` (`Roll`, `Name`, `Class`, `Marks`) VALUES
 (680578, 'Fahim', 4, 85),
 (680594, 'Tanbir', 4, 80),
 (680602, 'Hasan', 4, 82),
 (680572, 'Shamim', 4, 85);
 COMMIT;
-
